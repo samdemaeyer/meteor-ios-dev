755dcf99668e178945e24597638f73ec8923a1e1
Arek Komarnicki <ar1s336@gmail.com>
2015-03-24 15:15:03 +0000

    pulled from nitrous

---
+++ server/fixtures.js
@@ -0,0 +1,19 @@
+if (Posts.find().count() === 0) {
+    Posts.insert({
+        title: 'Tahini & lemon sauce',
+        url: 'http://www.bbcgoodfood.com/recipes/tahini-lemon-sauce',
+        imageUrl: 'http://www.bbcgoodfood.com/sites/bbcgoodfood.com/files/recipe_images/tahini-lemon-sauce.jpg'
+    });
+
+    Posts.insert({
+        title: 'Rose baklava',
+        url: 'http://www.bbcgoodfood.com/recipes/rose-baklava',
+        imageUrl: 'http://www.bbcgoodfood.com/sites/bbcgoodfood.com/files/recipe_images/rose-baklava.jpg'
+    });
+
+    Posts.insert({
+        title: 'Duck egg sponge cake',
+        url: 'http://www.bbcgoodfood.com/recipes/duck-egg-sponge-cake',
+        imageUrl: 'http://www.bbcgoodfood.com/sites/bbcgoodfood.com/files/recipe_images/duck-egg-sponge-cake.jpg'
+    });
+}
