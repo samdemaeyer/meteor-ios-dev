0080a4b30199977391c2d776ff267430a0c78af3
Arek Komarnicki <ar1s336@gmail.com>
2015-03-26 17:55:00 +0000

    removed schema for now (come back to that later) :soon:

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,11 +1,32 @@
-AutoForm.hooks({
-  'add-form': {
-    onSuccess: function (operation, result, template) {
-      console.log('Post saved successfully!');
-    },
+Template.postSubmit.events({
+    'submit form': function(e) {
+        e.preventDefault();
 
-    onError: function(operation, error, template) {
-      console.log(error);
+        var post = {
+            url: $(e.target).find('[name=url]').val(),
+            title: $(e.target).find('[name=title]').val()
+        };
+
+        Meteor.call('postInsert', post, function(error, result) {
+            
+            if (error) {
+                return alert(error.reason);
+            }
+
+            if (result.postExists) {
+                IonPopup.alert({
+                    title: 'Opps!',
+                    template: 'That recipe already exists.',
+                    okText: 'Got it Chef!',
+                });
+            }
+
+            else {
+                Router.go('postPage', {
+                    _id: result._id
+                });
+            }
+
+        });
     }
-  }
 });
