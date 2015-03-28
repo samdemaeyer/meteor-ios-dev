96d355787e3ac426f16a07f507b51bb3d6b83e92
Arek Komarnicki <ar1s336@gmail.com>
2015-03-28 20:09:17 +0000

    :beer:

--- server/fixtures.js
+++ server/fixtures.js
@@ -16,4 +16,22 @@ if (Posts.find().count() === 0) {
         url: 'http://www.bbcgoodfood.com/recipes/duck-egg-sponge-cake',
         imageUrl: 'http://www.bbcgoodfood.com/sites/bbcgoodfood.com/files/recipe_images/duck-egg-sponge-cake.jpg'
     });
+
+    Posts.insert({
+        title: 'Rose baklava',
+        url: 'http://www.bbcgoodfood.com/recipes/rose-baklava',
+        imageUrl: 'http://www.bbcgoodfood.com/sites/bbcgoodfood.com/files/recipe_images/rose-baklava.jpg'
+    });
+
+    Posts.insert({
+        title: 'Tahini & lemon sauce',
+        url: 'http://www.bbcgoodfood.com/recipes/tahini-lemon-sauce',
+        imageUrl: 'http://www.bbcgoodfood.com/sites/bbcgoodfood.com/files/recipe_images/tahini-lemon-sauce.jpg'
+    });
+
+    Posts.insert({
+        title: 'Duck egg sponge cake',
+        url: 'http://www.bbcgoodfood.com/recipes/duck-egg-sponge-cake',
+        imageUrl: 'http://www.bbcgoodfood.com/sites/bbcgoodfood.com/files/recipe_images/duck-egg-sponge-cake.jpg'
+    });
 }
