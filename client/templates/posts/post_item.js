bb507122d65bb97b1b6a467f6e06e11ebe87d4b9
Arek <ar1s336@gmail.com>
2015-03-28 21:03:23 +0000

    added permissions + username helper! :feelsgood

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,7 +1,16 @@
 Template.postItem.helpers({
+    ownPost: function() {
+        return this.userId === Meteor.userId();
+    },
     domain: function() {
         var a = document.createElement('a');
         a.href = this.url;
         return a.hostname;
+    },
+    author: function() {
+        var author = this.author;
+        var getName = author.match(/^([^@]*)@/);
+        var username = getName ? getName[1] : null;
+        return username;
     }
 });
